7b8eb04728462560e058a88295b63e3a7f714c70
Júnior <valdecyborgesjr@hotmail.com>
2021-02-27 23:43:12 -0300

    Set up CompletedChallenges

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -1,9 +1,14 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/CompletedChallenges.module.css';
 
 export const CompletedChallenges: React.FC = () => {
-    return (
-        <div className={styles.completedChallengesContainer}>
-            <span>Desafios Completos</span> <span>5</span>
-        </div>
-    );
+  const { challengesCompleted } = useContext(ChallengesContext);
+
+
+  return (
+      <div className={styles.completedChallengesContainer}>
+          <span>Desafios Completos</span> <span>{challengesCompleted}</span>
+      </div>
+  );
 }
